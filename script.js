29a3760c4bbf6e73fbeb1a64e8fbbd6803ecfe66
Leon Hlabathi <114271403+itsleonbro@users.noreply.github.com>
2024-08-31 22:42:42 +0200

    added a function to delete the last character on every button click

--- script.js
+++ script.js
@@ -8,3 +8,12 @@ function clearDisplay() {
   screen.value = "";
 }
 
+function deleteOneValue() {
+  // get the current value of the display
+  const currentValue = screen.value;
+
+  // removes the last character if there's any
+  if (currentValue.length > 0) {
+    screen.value = currentValue.slice(0, -1);
+  }
+}
